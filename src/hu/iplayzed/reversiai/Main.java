d5c08bc67af2fc3c2837bae934e63bf6eb2fdc93
Börcsök Balázs Róbert <pauljouser@gmail.com>
2021-10-27 11:07:01 +0200

    Added package prepend to argument.
    Signed-off-by: IPlayZed <pauljouser@gmail.com>

--- src/hu/iplayzed/reversiai/Main.java
+++ src/hu/iplayzed/reversiai/Main.java
@@ -9,7 +9,7 @@ public class Main {
     private static void testEngine() throws Exception {
         String seed = Integer.toString(ThreadLocalRandom.current().nextInt());
         String[] engineArgs = {"0", "game.oth.OthelloGame", seed, "10", "3", "2000", "game.oth.players.GreedyPlayer",
-                "SamplePlayer"};
+                "hu.iplayzed.reversiai.SamplePlayer"};
         Engine.main(engineArgs);
     }
 
