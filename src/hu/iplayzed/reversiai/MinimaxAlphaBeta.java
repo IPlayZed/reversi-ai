adc8b24012a4adf74268092e94f6921f8245f52d
Börcsök Balázs Róbert <pauljouser@gmail.com>
2021-10-31 15:02:49 +0100

    Continued implementing based on GreedyPlayer.
    Signed-off-by: IPlayZed <pauljouser@gmail.com>

--- src/hu/iplayzed/reversiai/MinimaxAlphaBeta.java
+++ src/hu/iplayzed/reversiai/MinimaxAlphaBeta.java
@@ -1,11 +1,16 @@
 package hu.iplayzed.reversiai;
 
 import game.oth.OthelloAction;
+import game.oth.OthelloGame;
 import game.oth.OthelloPlayer;
 
+import java.util.ArrayList;
 import java.util.Random;
 
+@SuppressWarnings("unused")
 public class MinimaxAlphaBeta extends OthelloPlayer {
+
+    private ArrayList<OthelloAction> othelloActions = new ArrayList<>();
 
     /**
      * Specifies the class of a player, that is represented by the specified color
@@ -17,11 +22,22 @@ public class MinimaxAlphaBeta extends OthelloPlayer {
      * @param random random number generator
      */
     public MinimaxAlphaBeta(int color, int[][] board, Random random) {
+
         super(color, board, random);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                othelloActions.add(new OthelloAction(i, j));
+            }
+        }
+    }
+
+    private int updateColor() {
+        return 1 - color;
     }
 
     @Override
     public OthelloAction getAction(OthelloAction prevAction, long[] remainingTimes) {
+        if (prevAction != null) OthelloGame.setAction(board, prevAction.i, prevAction.j, updateColor());
         return null;
     }
 }
